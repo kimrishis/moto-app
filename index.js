a6bed8a018c35a7b1e1d00a41c11a54bc95170e1
kimrishis <97913117+kimrishis@users.noreply.github.com>
2022-06-18 23:20:06 -0300

    feat: carregando dados por localStorage

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 var pecas = []
+
+window.onload = function(){
+    exibirPecas()
+}
+
 function hodometro(){
     let hodo = document.getElementById("inputhodo").value
     let containerMsg = document.getElementById("containermanutencao")
@@ -15,12 +20,14 @@ function cadastraPeca(){
     let peca = document.getElementById("inputpeca").value
     let pecakm = document.getElementById("inputpecakm").value
     pecas.push({nome:peca,km:pecakm})
+    localStorage.setItem('pecas', JSON.stringify(pecas))
     exibirPecas()
     document.getElementById("inputpeca").value = ""
     document.getElementById("inputpecakm").value = ""
 }
 
 function exibirPecas(){
+    carregarPecas()
     let containerMsg = document.getElementById("conainerPecas")
     containerMsg.innerHTML = ``
     for (const objpeca of pecas) {
@@ -29,4 +36,14 @@ function exibirPecas(){
         `
     }
     containerMsg.style = ""
+}
+
+function carregarPecas(){
+    const valoresGuardados = localStorage.getItem('pecas')
+    if (valoresGuardados) {
+        pecas = JSON.parse(valoresGuardados)
+    } else {
+        console.warn('vamos sem carregar pecas')
+        pecas = []
+    }
 }
